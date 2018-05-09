fec2e3780982293fbc1b8ce2f814ab67b395ff74
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-05-09 15:47:01 +0530

    Update CuTestTest.c

--- test/CuTestTest.c
+++ test/CuTestTest.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "lib/CuTest.h"
+#include "../lib/CuTest.h"
 
 /*-------------------------------------------------------------------------*
  * Helper functions
