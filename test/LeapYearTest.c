7e8c0c9e9621dcccea2f77b05ea192b1cb47f5d0
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-05-14 17:06:16 +0530

    Create LeapYearTest.c

---
+++ test/LeapYearTest.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <setjmp.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../lib/CuTest.h"
+#include "../src/leap-year/leap-year.h"
+
+
+/*-------------------------------------------------------------------------*
+ * Leap Year Test Suite
+ *-------------------------------------------------------------------------*/
+
+void TestIsLeapYearPassCases(CuTest* tc)
+{
+	CuAssertTrue(tc, isLeapYear(2008));
+	CuAssertTrue(tc, isLeapYear(2400));
+}
+
+void TestIsLeapYearFailCases(CuTest* tc)
+{
+	CuAssertTrue(tc, !isLeapYear(1900));
+	CuAssertTrue(tc, !isLeapYear(1563));
+}
